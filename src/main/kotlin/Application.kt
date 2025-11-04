5c73845860bf154d416efb9d407059319edd4ecf
PollemAnt <82649672+PollemAnt@users.noreply.github.com>
2025-11-04 15:53:47 +0100

    Adds polymorphic serialization for the `MoveFeedback` sealed class.

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import com.example.models.MoveFeedback
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
@@ -10,6 +11,7 @@ import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.cors.routing.CORS
 import io.ktor.server.websocket.WebSockets
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.*
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -25,6 +27,22 @@ fun Application.module() {
     install(ContentNegotiation) {
         json(Json {
             classDiscriminator = "type"
+            prettyPrint = true
+            isLenient = true
+
+            serializersModule = SerializersModule {
+                polymorphic(MoveFeedback::class) {
+                    subclass(MoveFeedback.Standard::class)
+                    subclass(MoveFeedback.GuardPlayed::class)
+                    subclass(MoveFeedback.BaronPlayed::class)
+                    subclass(MoveFeedback.HandmaidPlayed::class)
+                    subclass(MoveFeedback.KingPlayed::class)
+                    subclass(MoveFeedback.PrincePlayed::class)
+                    subclass(MoveFeedback.PriestPlayed::class)
+                    subclass(MoveFeedback.SpyPlayed::class)
+                    subclass(MoveFeedback.ChancellorPlayed::class)
+                }
+            }
         })
     }
 
