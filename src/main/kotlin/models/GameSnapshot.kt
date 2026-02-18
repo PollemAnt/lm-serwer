258356b2fab3ab74869d0971ecb0d621f5930d1d
PollemAnt <82649672+PollemAnt@users.noreply.github.com>
2026-02-18 16:52:13 +0100

    Make activePlayerId non-nullable

--- src/main/kotlin/models/GameSnapshot.kt
+++ src/main/kotlin/models/GameSnapshot.kt
@@ -6,7 +6,5 @@ import kotlinx.serialization.Serializable
 data class GameSnapshot(
     val players: List<Player>,
     val cards: List<Card>,
-    val activePlayerId: Int?
+    val activePlayerId: Int
 )
-
-val gameSnapshots = mutableListOf<GameSnapshot>()
